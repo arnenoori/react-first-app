d475636fdf12b5cddcb96feabc4d8c027f607e15
Arne <arnenoori@gmail.com>
2023-10-01 21:14:03 -0700

    final update

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,31 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "./Table";
-
-const characters = [
-    {
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ];
+import Form from './Form';
 
 function MyApp() {
-    return (
+  const [characters, setCharacters] = useState([]);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+
+  // New function to update the character list
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
+  return (
     <div className="container">
-        <Table characterData={characters} />
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
     </div>
-    );
+  );
 }
 
 export default MyApp;
