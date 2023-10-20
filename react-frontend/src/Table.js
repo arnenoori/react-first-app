58075995be4b98b853fdda75f01e3c90a5e006db
Arne <arnenoori@gmail.com>
2023-10-20 10:33:28 -0700

    delete id button

--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -16,25 +16,21 @@ import React from "react";
   
   function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
-        return (
-            <tr key={index}>
-                <td>{row.id}</td>
-                <td>{row.name}</td>
-                <td>{row.job}</td>
-                <td>
-                    <button onClick={() => props.removeCharacter(index)}>
-                        Delete
-                    </button>
-                    <button onClick={() => props.removeCharacterById(row.id)}>
-                        Delete by ID
-                    </button>
-                </td>
-            </tr>
-        );
+      return (
+        <tr key={index}>
+          <td>{row.id}</td>
+          <td>{row.name}</td>
+          <td>{row.job}</td>
+          <td>
+            <button onClick={() => props.removeCharacterById(row.id)}>
+              Delete
+            </button>
+          </td>
+        </tr>
+      );
     });
     return <tbody>{rows}</tbody>;
-}
-
+  }
   
 function Table (props) {
   return (
